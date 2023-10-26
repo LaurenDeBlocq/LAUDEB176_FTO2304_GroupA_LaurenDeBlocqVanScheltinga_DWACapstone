a35add6e13b429e77109ba8385a75d15ec206892
Lauren de Blocq <lauren.deblocq@gmail.com>
2023-10-26 23:38:58 +0200

    Created basic outline and styling of Show page

---
+++ Capstone-Podcast-App/src/components/Season.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Season() {
+    return (
+        <main >
+            <img />
+            <h1>{data.title}</h1>
+            
+        </main>
+    )
+}
+
+export default Season
--- Capstone-Podcast-App/src/routes/ShowPage.jsx
+++ Capstone-Podcast-App/src/routes/ShowPage.jsx
@@ -1,11 +1,62 @@
-import React from "react"
+import {React, useEffect} from "react"
 import { useParams } from "react-router-dom"
+import '../App.css'
+
+import Header from "../components/Header";
+import Season from "../components/Season";
 
 function Show() {
     const {showId} = useParams()
     
+    const [data, setData] = React.useState(null);
+    const [loading, setLoading] = React.useState(true);
+
+    
+    const [seasonSelect, setSeasonSelect ] = React.useState(0);
+  
+
+    useEffect(() => {
+        fetch(`https://podcast-api.netlify.app/id/${showId}`)
+        .then(response => response.json())
+        .then(data => {
+            setData(data);
+            setLoading(false);
+        })
+        .catch(error => console.error('Error:', error));
+        }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    
+    const seasonChoice = data.seasons.map( (season) => {
+        return (
+        <option value={season.season}>{season.season}</option>)
+    })
+
+    console.log(seasonSelect);
+    const handleSeasonSelect = () => {
+        setSeasonSelect(value)
+        console.log(seasonSelect);
+    }
+
     return (
-        <h1>Sanity checks all round the show {showId}</h1>
+        <div className="show">
+            <Header />
+
+            <h1>{data.title}</h1>
+            <img src={data.image} className="show--image" />
+            <p className="show--description">{data.description}</p>
+            <div className="season--selector">
+                <p>Season:</p>
+                <select onChange={handleSeasonSelect} >
+                    {seasonChoice}
+                </select>
+            </div>
+            <Season  />
+            {/* <Season data={data} /> */}
+        </div>
     )
 }
 
