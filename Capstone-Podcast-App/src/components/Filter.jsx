974177e263969a0373c1b7c78369c5a539783169
Lauren de Blocq <lauren.deblocq@gmail.com>
2023-10-27 13:39:10 +0200

    Styled audio player and filter

--- Capstone-Podcast-App/src/components/Filter.jsx
+++ Capstone-Podcast-App/src/components/Filter.jsx
@@ -4,10 +4,25 @@ export default function Filter() {
     const dropDownOptions = ["A-Z", "Z-A", "Newest", "Oldest"]
     
     const dropDown = dropDownOptions.map(option => {
-        
+        return( <option value={option} key={option}>{option}</option> )
     })
 
+    const handleFilterSelect = () => {
+        console.log("selected");
+    }
+
     return(
-        <h1>Filter goes here</h1>
+        <div className="filter">
+            <div className="filter--select">
+                <label >Filter by:</label>
+                <select defaultValue={""} onChange={handleFilterSelect} >
+                    {dropDown}
+                </select>
+            </div>
+            <div className="filter--search">
+                <label htmlFor="stringSearch">Or Search for:</label>
+                <input type="text" id="stringSearch" name="stringSearch" />
+            </div>
+        </div>
     )
 }
